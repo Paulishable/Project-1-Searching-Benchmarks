d33af0d352be3205d2110a398431cfac6a8b158d
Paulishable <paulzelenski@msn.com>
2022-05-17 06:51:49 -0700

    Added Fibonacci Search

--- Main.py
+++ Main.py
@@ -3,6 +3,32 @@ import math
 from time import perf_counter
 
 
+def fibonacci_search(lys, val):
+    fibM_minus_2 = 0
+    fibM_minus_1 = 1
+    fibM = fibM_minus_1 + fibM_minus_2
+    while fibM < len(lys):
+        fibM_minus_2 = fibM_minus_1
+        fibM_minus_1 = fibM
+        fibM = fibM_minus_1 + fibM_minus_2
+    index = -1
+    while fibM > 1:
+        i = min(index + fibM_minus_2, (len(lys)-1))
+        if lys[i] < val:
+            fibM = fibM_minus_1
+            fibM_minus_1 = fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+            index = i
+        elif lys[i] > val:
+            fibM = fibM_minus_2
+            fibM_minus_1 = fibM_minus_1 - fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+        else :
+            return True
+    if fibM_minus_1 and index < (len(lys) - 1) and lys[index + 1] == val:
+        return True
+    return False
+
 # Jump Search
 def jump_search(a_lyst, target):
     # Finding block size to be jumped
@@ -66,6 +92,12 @@ def binary_search(a_lyst, target):
     return False
 
 
+#  SET UP THE DATA LIST TO SORT..............
+# DATA_SIZE = 100_000_000
+# random.seed(0)
+# DATA = random.sample(range(DATA_SIZE * 3), k=DATA_SIZE)
+# huge_lyst = sorted(DATA)
+
 # Create a list of Random Numbers...
 
 random.seed(123987)
@@ -81,7 +113,7 @@ x_max = lyst[-1]
 x_mid = lyst[num // 2]
 x_not_present = -1
 
-n = 3
+n = 4
 m = 4
 timing_results = [[0] * m for i in range(n)]
 
@@ -108,6 +140,13 @@ def do_the_searches(the_target, test_id):
     jump_search_time = jump_search_end - jump_search_start
     timing_results[2][test_id] = jump_search_time
 
+    fibonacci_search_start = perf_counter()
+    result = fibonacci_search(lyst, the_target)
+    fibonacci_search_end = perf_counter()
+
+    fibonacci_search_time = fibonacci_search_end - fibonacci_search_start
+    timing_results[3][test_id] = fibonacci_search_time
+
 
 the_target = x_min
 do_the_searches(the_target, 0)
@@ -121,10 +160,12 @@ do_the_searches(the_target, 2)
 the_target = x_not_present
 do_the_searches(the_target, 3)
 
-print(f"\n  ALGORITHM    First Element   Middle Element   Last Element   Not an element")
-print("Linear Search", "    %.3e" % timing_results[0][0], "      %.3e" % timing_results[0][1],
-      "      %.3e" % timing_results[0][2], "     %.3e" % timing_results[0][3])
-print("Binary Search", "    %.3e" % timing_results[1][0], "      %.3e" % timing_results[1][1],
-      "      %.3e" % timing_results[1][2], "     %.3e" % timing_results[1][3])
-print("  Jump Search", "    %.3e" % timing_results[2][0], "      %.3e" % timing_results[2][1],
-      "      %.3e" % timing_results[2][2], "     %.3e" % timing_results[2][3])
+print(f"\n  ALGORITHM      First Element   Middle Element    Last Element   Not an element")
+print("Linear Search", "    %.8f" % timing_results[0][0], "      %.8f" % timing_results[0][1],
+      "      %.8f" % timing_results[0][2], "     %.8f" % timing_results[0][3])
+print("Binary Search", "    %.8f" % timing_results[1][0], "      %.8f" % timing_results[1][1],
+      "      %.8f" % timing_results[1][2], "     %.8f" % timing_results[1][3])
+print("  Jump Search", "    %.8f" % timing_results[2][0], "      %.8f" % timing_results[2][1],
+      "      %.8f" % timing_results[2][2], "     %.8f" % timing_results[2][3])
+print("  Fib Search ", "    %.8f" % timing_results[3][0], "      %.8f" % timing_results[3][1],
+      "      %.8f" % timing_results[3][2], "     %.8f" % timing_results[3][3])
